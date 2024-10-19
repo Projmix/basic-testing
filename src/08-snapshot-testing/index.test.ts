983bfaa1f74e23b1e827a093429751a8a62031ec
projmix <balb310372@mail.ru>
2024-10-19 14:48:49 +0300

    add snapshot testing

--- src/08-snapshot-testing/index.test.ts
+++ src/08-snapshot-testing/index.test.ts
@@ -1,14 +1,32 @@
-// Uncomment the code below and write your tests
-// import { generateLinkedList } from './index';
+import { generateLinkedList } from './index';
 
 describe('generateLinkedList', () => {
-  // Check match by expect(...).toStrictEqual(...)
-  test('should generate linked list from values 1', () => {
-    // Write your test here
+  test('should generate linked list from values using strict comparison', () => {
+    const input = [1, 2, 3, 4];
+    const expectedOutput = {
+      value: 1,
+      next: {
+        value: 2,
+        next: {
+          value: 3,
+          next: {
+            value: 4,
+            next: {
+              value: null,
+              next: null,
+            },
+          },
+        },
+      },
+    };
+
+    const result = generateLinkedList(input);
+    expect(result).toStrictEqual(expectedOutput);
   });
 
-  // Check match by comparison with snapshot
-  test('should generate linked list from values 2', () => {
-    // Write your test here
+  test('should generate linked list from values and match snapshot', () => {
+    const input = [1, 2, 3, 4];
+    const result = generateLinkedList(input);
+    expect(result).toMatchSnapshot();
   });
 });
